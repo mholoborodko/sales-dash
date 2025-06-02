4daa568ee459829c617968ecf17a205fd872abce
Mykola Holoborodko <nik.holo92@gmail.com>
2025-06-02 17:22:33 +0300

    fix: improve icon component

--- src/shared/ui/Icon/index.tsx
+++ src/shared/ui/Icon/index.tsx
@@ -7,20 +7,26 @@ interface IconProps {
   className?: string;
 }
 
+const icons = import.meta.glob('@/shared/assets/icons/*.svg', { as: 'raw' });
+
 export const Icon: React.FC<IconProps> = ({ name, size = 24, className }) => {
   const [svg, setSvg] = useState<string | null>(null);
 
   useEffect(() => {
     const loadSvg = async () => {
-      const response = await fetch(`src/shared/assets/icons/${name}.svg`);
-      if (response.ok) {
-        const svgText = await response.text();
+      const importSvg = icons[`/src/shared/assets/icons/${name}.svg`];
+
+      if (importSvg) {
+        const svgText = await importSvg();
         setSvg(
           svgText.replace(
             /(<svg[^>]+)>/,
             `$1 width="${size}" height="${size}" fill="currentColor">`
           )
         );
+      } else {
+        // eslint-disable-next-line no-console
+        console.warn(`SVG icon not found: ${name}`);
       }
     };
 
