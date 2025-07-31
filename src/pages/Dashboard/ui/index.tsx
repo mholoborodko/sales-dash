25dcb7ec21d7f55ff6de7efef2cc83e3bac694eb
Mykola Holoborodko <nik.holo92@gmail.com>
2025-07-31 17:16:16 +0300

    feat: imporve sidebar responsive

--- src/pages/Dashboard/ui/index.tsx
+++ src/pages/Dashboard/ui/index.tsx
@@ -8,6 +8,8 @@ import {
   ResponsiveContainer,
 } from 'recharts';
 
+import { Button } from '@/shared/ui';
+
 const data = [
   { name: 'Jan', sales: 400 },
   { name: 'Feb', sales: 300 },
@@ -18,24 +20,31 @@ const data = [
 
 export const DashboardPage = () => {
   return (
-    <div className="grid grid-cols-2 gap-[50px]">
-      {[1, 2, 3, 4].map((_, index) => (
-        <div
-          key={index}
-          className="w-full h-[300px] bg-white rounded-xl shadow-md p-10"
-        >
-          <h2 className="text-lg font-semibold mb-4">Monthly Sales</h2>
-          <ResponsiveContainer height="100%" width="100%">
-            <BarChart data={data}>
-              <CartesianGrid stroke="#e5e7eb" strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Bar dataKey="sales" fill="#6366F1" radius={[4, 4, 0, 0]} />
-            </BarChart>
-          </ResponsiveContainer>
-        </div>
-      ))}
+    <div className="flex flex-col gap-3">
+      <div className="flex items-center justify-between">
+        <h2 className="text-lg font-semibold">Hello, John Smith</h2>
+        <Button label="Download PDF" />
+      </div>
+
+      <div className="grid grid-cols-2 gap-8">
+        {[1, 2, 3, 4].map((_, index) => (
+          <div
+            key={index}
+            className="w-full h-[300px] bg-white rounded-xl shadow-md p-10"
+          >
+            <h2 className="text-lg font-semibold mb-4">Monthly Sales</h2>
+            <ResponsiveContainer height="100%" width="100%">
+              <BarChart data={data}>
+                <CartesianGrid stroke="#e5e7eb" strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey="sales" fill="#6366F1" radius={[4, 4, 0, 0]} />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/widgets/Sidebar/ui/index.tsx
+++ src/widgets/Sidebar/ui/index.tsx
@@ -3,7 +3,8 @@ import { FC } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 import logo from '@/shared/assets/icons/logo.svg';
-import { Icon } from '@/shared/ui';
+import smallLogo from '@/shared/assets/icons/small-logo.svg';
+import { Avatar, Icon } from '@/shared/ui';
 
 export interface SidebarItem {
   label: string;
@@ -19,9 +20,11 @@ export const Sidebar: FC<SidebarProps> = ({ items }) => {
   const location = useLocation();
 
   return (
-    <aside className="sticky top-0 h-screen w-60 bg-[#1f1b2e] text-white p-4 shadow-lg">
-      <img alt="logo" src={logo} width={150} />
-      <nav className="mt-8 flex flex-col gap-2">
+    <aside className="sticky top-0 h-screen w-20 lg:w-60 bg-[#1f1b2e] text-white p-4 shadow-lg flex flex-col">
+      <img alt="logo" className="hidden lg:block" src={logo} width={150} />
+      <img alt="logo" className="lg:hidden block" src={smallLogo} width={48} />
+
+      <nav className="mt-8 flex flex-col flex-1 gap-2">
         {items.map(item => {
           const isActive = location.pathname === item.to;
 
@@ -35,11 +38,23 @@ export const Sidebar: FC<SidebarProps> = ({ items }) => {
               to={item.to}
             >
               <Icon name={item.icon} size={18} />
-              <span>{item.label}</span>
+              <span className="hidden lg:block">{item.label}</span>
             </Link>
           );
         })}
       </nav>
+
+      <div className="flex items-center gap-3 mb-4">
+        <Avatar size={48} />
+        <div className="flex flex-col gap-1">
+          <span className="text-gray-200 text-sm hidden lg:block">
+            John Smith
+          </span>
+          <span className="text-gray-400 text-xs hidden lg:block">
+            johnsmith@gmail.com
+          </span>
+        </div>
+      </div>
     </aside>
   );
 };
